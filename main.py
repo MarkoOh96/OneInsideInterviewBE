712645e450e4579001f59099a9a0326226d3c878
unknown <markopoposki96@gmail.com>
2023-05-13 15:08:08 +0200

    project developer routes

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ from model import db
 from routes.client_routes import client_routes
 from routes.developer_routes import developer_routes
 from routes.project_routes import project_routes
+from routes.project_developers_resources import project_developer_routes
 import requests
-from models.clients import Client
-from models.developers import Developer
-from models.projects import Project
-from models.projects_developers import ProjectDeveloper
 
 
 app = Flask(__name__)
@@ -18,6 +15,7 @@ db.init_app(app)
 app.register_blueprint(client_routes)
 app.register_blueprint(developer_routes)
 app.register_blueprint(project_routes)
+app.register_blueprint(project_developer_routes)
 
 
 if __name__ == '__main__':
